1757c00b43fb2a9446c563af33f054be6e338002
Jeffrey Hobbs <hobbs@users.sourceforge.net>
2002-12-23 23:36:26 +0000

    stub for plugin installer (does nothing now)

---
+++ tclplugin/tools/install.tcl
@@ -0,0 +1,17 @@
+# install.tcl --
+#
+#     Installer for the Tcl plugin.
+#
+# CONTACT:      tclplugin-core@lists.sourceforge.net
+#
+# Copyright (c) 2002 by ActiveState Corporation.
+#
+# RCS:  @(#) $Id$
+
+set supportEmail "tclplugin-core@lists.sourceforge.net"
+set supportUrl   "http://www.tcl.tk/software/plugin/"
+
+error "
+OK, so this doesn't do anything yet.  See README.binary for what
+this should do when it really wants to install stuff.
+"

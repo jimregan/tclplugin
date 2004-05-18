e551d04de8d8ef846d2d08d2141aba280cb248a0
Jeffrey Hobbs <hobbs@users.sourceforge.net>
2004-05-18 17:08:14 +0000

    * library/remoted.tcl: require at least Tcl/Tk 8.2 for the 	external process (may force 8.4). 	Remove vwait at end, it was ineffective since this was launched 	with Tk loaded.

--- tclplugin/library/remoted.tcl
+++ tclplugin/library/remoted.tcl
@@ -19,8 +19,8 @@
 # Called with the following arguments:
 # argv[0] == port number to connect to on the local host.
 
-package require Tcl 8.0
-package require Tk 8.0
+package require Tcl 8.2
+package require Tk 8.2
 
 # Set our base name (used for error reporting) if it was not set yet.
 
@@ -33,8 +33,22 @@ if {![info exists ::Name]} {
 set plugin(library) [file dirname [info script]]
 set plugin(topdir)  $plugin(library)
 
-# The following procedure initializes the server:
+# This procedure is used by commands that execute code in the spawning
+# process:
+set ::msgNum   0
+proc pnExecute {cmd key aList} {
+    incr ::msgNum
+    ::rpi::invoke $::Cli "::pn$cmd $key $aList"
+}
 
+# We redefine bgerror to log messages, because we may not have any
+# visible presence.
+proc bgerror {msg} {
+    ::pluglog::log {} "bgerror $msg ($::errorInfo)" ERROR
+    puts stderr "BgError: $msg\n$::errorInfo"
+}
+
+# The following procedure initializes the server:
 proc remotedInit {} {
     global argc argv env auto_path errorInfo plugin tk_version tk_library
 
@@ -61,29 +75,6 @@ proc remotedInit {} {
 	exit
     }
 
-    if {![string match "*Plugin*" $::tcl_patchLevel]} {
-	::pluglog::log {} "Likely failure upcoming because of invalid tcl\
-		version: $::tcl_patchLevel" WARNING
-    }
-
-    if {![file exists [file join $tk_library safetk.tcl]]} {
-	set p $tk_library
-	set tk_library [file join $plugin(topdir) $tkdir]
-	if {[string compare $p $tk_library] == 0} {
-	    set msg "Installation problem: can't find safetk.tcl in \"$p\""
-	    ::pluglog::log {} $msg ERROR
-	    NotifyError "Fatal" $msg
-	    exit -1
-	}
-	::pluglog::log {} "no safetk.tcl in \"$p\", switching to \"$tk_library\"!" WARNING
-	if {![file exists [file join $tk_library safetk.tcl]]} {
-	    ::pluglog::log {} "no safetk.tcl in $tk_library either! aborting" ERROR
-	    NotifyError "Fatal" "can't find safetk.tcl in \"$p\"\
-		    nor in\ \"$tk_library\" misconfiguration somewhere..."
-	    exit -1
-	}
-    }
-
     ::pluglog::log {} "AutoPath = $auto_path"
 
     # The correct value has been set by our caller:
@@ -93,20 +84,17 @@ proc remotedInit {} {
 
     # And we also need the browser package which implements the
     # browser specific stuff:
-
     package require browser 1.0
 
     # Connect to our spawner:
-
-    package require rpi 1.0;
+    package require rpi 1.0
     if {[catch {set ::Cli [::rpi::newClient\
 	    localhost [lindex $argv 0] localhost]} msg]} {
 	puts stderr "FATAL: wishd.tcl: Can not connect back : $msg"
 	exit
     }
 
-    # Install our own limk down handler
-
+    # Install our own link down handler
     proc ::rpi::linkDown {args} {
 	::pluglog::log {} "LinkDown ($args): bye bye !" WARNING
 	set ::Exiting 1
@@ -115,36 +103,11 @@ proc remotedInit {} {
 
     # Initialize 'browser' (wherever it has been effectively installed)
     # (common inproc/outproc browser specific init)
-
     ${::cfg::implNs}::init
 
 }
 
-# This procedure is used by commands that execute code in the spawning
-# process:
-
-set ::msgNum   0
-proc pnExecute {cmd key aList} {
-    incr ::msgNum
-    ::rpi::invoke $::Cli "::pn$cmd $key $aList"
-}
-
-# We redefine bgerror to log messages, because we may not have any
-# visible presence.
-
-proc bgerror {msg} {
-    ::pluglog::log {} "bgerror $msg ($::errorInfo)" ERROR
-    puts stderr "BgError: $msg\n$::errorInfo"
-}
-
-
 # Initialize everything:
-
 remotedInit
 
 ::pluglog::log {} "remoted.tcl init done"
-
-# Now wait forever (or at least until someone set the ::Exiting variable).
-
-vwait ::Exiting
-

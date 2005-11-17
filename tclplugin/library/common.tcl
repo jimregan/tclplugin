2f16f5fd0ec87c6946e07b4fc0a9d7eeaa68930d
Jeffrey Hobbs <hobbs@users.sourceforge.net>
2005-11-17 19:28:20 +0000

    (SetupConsole): fix tkcon init

--- tclplugin/library/common.tcl
+++ tclplugin/library/common.tcl
@@ -8,7 +8,7 @@
 #
 # Copyright (c) 1996-1997 Sun Microsystems, Inc.
 # Copyright (c) 2000 by Scriptics Corporation.
-# Copyright (c) 2002-2004 ActiveState Corporation.
+# Copyright (c) 2002-2005 ActiveState Corporation.
 #
 # See the file "license.terms" for information on usage and redistribution
 # of this file, and for a DISCLAIMER OF ALL WARRANTIES.
@@ -22,9 +22,7 @@ package provide plugin::common 1.0
 package require pluglog 1.1
 
 # If no widget have been created, we withdraw the unneeded .
-if {![llength [winfo children .]]} {
-    wm withdraw .
-}
+if {![llength [winfo children .]]} { wm withdraw . }
 
 # Setup and eventually start logging facility:
 proc SetupLogging {name} {
@@ -49,38 +47,33 @@ proc SetupConsole {} {
     global env plugin
 
     # Create a console if the user asks for it:
-    ::pluglog::log SetupConsole "Console setup"
-    if {[info exists env(TCL_PLUGIN_CONSOLE)]} {
-	if {[string is true $env(TCL_PLUGIN_CONSOLE)]} {
-	    # true value or "" means use built-in
-	    set consoleFile [file join $plugin(library) tkcon.tcl]
-	    set cmd {
-		namespace eval ::tkcon {}
-		set ::tkcon::PRIV(protocol) {tkcon hide}
-		set ::tkcon::OPT(exec) ""
-		tkcon show
-	    }
-	} else {
-	    set consoleFile $env(TCL_PLUGIN_CONSOLE)
-	}
-
-	# these may not exist
-	if {![info exists ::argv]} { set ::argv {}; set ::argc 0 }
-
-	if {[catch {uplevel \#0 [list source $consoleFile]} msg]} {
-	    NotifyError SetupConsole\
-		"SetupConsole loading \"$consoleFile\": $msg\n$::errorInfo"
-	    return
-	}
-	if {[info exists $cmd] && [catch {uplevel \#0 $cmd} msg]} {
-	    NotifyError SetupConsole\
-		"SetupConsole \"$cmd\": $msg\n$::errorInfo"
-	    return
-	}
-	::pluglog::log SetupConsole "Console file \"$consoleFile\" sourced ok"
-    } else {
-	::pluglog::log SetupConsole "No console created"
+    if {![info exists env(TCL_PLUGIN_CONSOLE)]} {
+	::pluglog::log SetupConsole "No plugin console requested"
+	return
     }
+
+    if {[string is true $env(TCL_PLUGIN_CONSOLE)]} {
+	# true value or "" means use built-in
+	set cmd {
+	    namespace eval ::tkcon {}
+	    set ::tkcon::PRIV(protocol) {tkcon hide}
+	    set ::tkcon::OPT(exec) ""
+	    source [file join $plugin(library) tkcon.tcl]
+	    tkcon show
+	}
+    } else {
+	set cmd [list source $env(TCL_PLUGIN_CONSOLE)]
+    }
+
+    # these may not exist
+    if {![info exists ::argv]} { set ::argv {}; set ::argc 0 }
+
+    if {[catch {uplevel \#0 $cmd} msg]} {
+	NotifyError SetupConsole\
+	    "SetupConsole:\n$cmd\n----\n$::errorInfo"
+	return
+    }
+    ::pluglog::log SetupConsole "Successfully create console"
 }
 
 
